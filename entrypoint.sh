9507604530ee43cadb134b018c002a5f752f0068
bart <barthex@users.noreply.github.com>
2023-10-03 18:50:33 +0200

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/libreoffice --nologo --norestore --invisible --nolockcheck --nodefault --headless "$@"
+/usr/bin/libreoffice --nologo --norestore --invisible --nolockcheck --nodefault --headless --nofirststartwizard --accept="socket,host=0.0.0.0,port=8100;urp"
 if [ -d "/tmp/hsperfdata_root" ]; then
 	rm -rf /tmp/hsperfdata_root
 fi
